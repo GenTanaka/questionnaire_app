0517838dc7d3a5931f081f16f824889b3a391f52
gentanaka <gen.tanaka2001@gmail.com>
2022-01-06 21:40:37 +0900

    add model
    モデル周りの追加

---
+++ database/migrations/2022_01_06_094331_create_answers_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnswersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('answers', function (Blueprint $table) {
+            $table->unsignedBigInteger('id', true);
+            $table->unsignedBigInteger('questionnaire_id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('answer1', 255);
+            $table->string('answer2', 255);
+            $table->softDeletes();
+            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
+            $table->foreign('questionnaire_id')->references('id')->on('questionnaires');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('answers');
+    }
+}
