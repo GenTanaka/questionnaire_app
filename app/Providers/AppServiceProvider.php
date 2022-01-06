a11741a3a505069a4714845471b613b2b91a1e59
gentanaka <gen.tanaka2001@gmail.com>
2022-01-07 08:09:27 +0900

    master

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,5 +27,8 @@ class AppServiceProvider extends ServiceProvider
     {
         Paginator::useBootstrap();
         Schema::defaultStringLength(191);
+        if (request()->isSecure()) {
+            \URL::forceScheme('https');
+        }
     }
 }
